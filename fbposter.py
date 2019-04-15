e7967bbc3a34ecabc9978175412a8614e7bacc40
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2019-04-16 00:28:20 +0600

    com

--- fbposter.py
+++ fbposter.py
@@ -7,20 +7,25 @@ def main():
     # your account user and password, path to image, links to groups and message
     usr = ""
     pwd = ""
-    message = ["https://www.fiverr.com/share/Boa7W",
-               "https://www.fiverr.com/share/lkzPA",
-               "https://www.fiverr.com/share/R9G8x",
-               "https://www.fiverr.com/share/WDmyx",
-               "https://www.fiverr.com/share/y8y0q"]
+    message = ["I Will Create Complete Custom Designed Wordpress Website. Please go to the following link https://www.fiverr.com/iamishraqhc/create-complete-custom-designed-wordpress-website?utm_campaign=gigs_show&utm_medium=shared&utm_source=copy_link&utm_term=boa7w",
+               "I Will Create An Ecommerce Website. Please go to the following link https://www.fiverr.com/iamishraqhc/create-an-ecommerce-website?utm_campaign=gigs_show&utm_medium=shared&utm_source=copy_link&utm_term=lkzpa",
+               "I Will Fix Any Wordpress Errors, Issues Or Problems. Please go to the following link https://www.fiverr.com/iamishraqhc/fix-any-wordpress-errors-issues-or-problems?utm_campaign=gigs_show&utm_medium=shared&utm_source=copy_link&utm_term=r9g8x",
+               "I Will Proofread And Edit Your Article In Less Than 24 Hours. Please go to the following link https://www.fiverr.com/iamishraqhc/proofread-and-edit-your-article-in-less-than-24-hours?utm_campaign=gigs_show&utm_medium=shared&utm_source=copy_link&utm_term=wdmyx",
+               "I Will Scrape, Extract, Mine Data From Any Web Into CSV And Excel. Please go to the following link https://www.fiverr.com/iamishraqhc/scrape-extract-mine-data-from-any-web-into-csv-and-excel?utm_campaign=gigs_show&utm_medium=shared&utm_source=copy_link&utm_term=y8y0q"]
     image_path = ""
     group_links = ['https://www.facebook.com/groups/121968305178198/',
                    'https://www.facebook.com/groups/762886083799826/',
                    'https://www.facebook.com/groups/769584619785908/',
                    'https://www.facebook.com/groups/getprojectsupwork/',
-                   'https://www.facebook.com/groups/fiverrhelpbangladesh/',
-                   'https://www.facebook.com/groups/shikhbeshobai/',
-                   'https://www.facebook.com/groups/474434189348407/',
-                   'https://www.facebook.com/groups/DSEntrepreneurs/']
+                   'https://www.facebook.com/groups/1515095995417244/',
+                   'https://www.facebook.com/groups/143184119689359/',
+                   'https://www.facebook.com/groups/Make.Money.Online.Club24/',
+                   'https://www.facebook.com/groups/makemoneyonlinee/',
+                   'https://www.facebook.com/groups/778428312272760/',
+                   'https://www.facebook.com/groups/212851892476720/',
+                   'https://www.facebook.com/groups/allfiverrhelp/',
+                   'https://www.facebook.com/groups/1048698711880571/',
+                   'https://www.facebook.com/groups/455654264473214/']
 
     # delete cache
     # profile = webdriver.FirefoxProfile()
